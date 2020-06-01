f43600b1c5849ff8110c95e69d26374734e18245
Romain Creniaux <romain.creniaux@gmail.com>
2020-06-01 21:50:14 +0200

    pcost first step

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+
+totalAmount = 0
+
+for line in f:
+    row = line.split(',')
+    totalAmount = totalAmount + (int(row[1]) * float(row[2]))
+
+f.close()
+
+print('Total cost is', totalAmount)
